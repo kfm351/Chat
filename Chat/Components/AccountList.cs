2d86bb0929cd651f02ce611314f7b52b0af93d39
kfm351 <kfm351@yandex.ru>
2024-04-22 00:49:13 +0300

    Добавьте файлы проекта.

---
+++ Chat/Components/AccountList.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BlazorApp2.Components
+{
+    public static class AccountList
+    {
+        public static List<User> Users { get; set; } = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("Components//Users.txt"));
+
+        public static void Get()
+        {
+            Users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("Components//Users.txt"));
+        }
+
+        public static void Update(User user)
+        {
+            Users.Add(user);
+            File.WriteAllText("Components//Users.txt", JsonConvert.SerializeObject(Users));
+        }
+    }
+}
---
+++ Chat/Components/User.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp2.Components
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Guid UserId { get; set; }
+
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+            UserId = Guid.NewGuid();
+        }
+    }
+}
